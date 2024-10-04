f2611e714e1e057d9fcb75bc227bf86770644d3a
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-10-04 10:03:47 -0600

    Create Create a Quiz.py

---
+++ Create a Quiz.py
@@ -0,0 +1,7 @@
+#Create your own quiz with five questions.                                     You can ask questions that require:
+#A number as an answer (e.g., What is 1+1?)
+#Text (e.g. What is Harry Potter's last name?)
+#True or False
+#A selection (Which of these choices are correct? A, B, or C?)
+#If the user answers a question correctly, print Correct. Else, print Incorrect and move on to the next question.
+#At the end of the program print how many questions they answered correctly.
